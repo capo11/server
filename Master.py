be5a3c39bc5d75551d5e71ba221b2b46868554ec
unknown <lorenzopisa00@gmail.com>
2017-09-13 11:16:46 +0100

    Controllo Ip per accesso funzionante

--- Master.py
+++ Master.py
@@ -10,7 +10,7 @@ import SocketServer
 HOST = "prova.it"
 PORT = 8000
 
-allowed_hosts = (('185.20.66.26', 8000),)
+allowed_hosts = '192.168.1.182'
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 httpd = SocketServer.TCPServer(("", PORT), Handler)
@@ -21,38 +21,10 @@ IP = socket.gethostbyname(hostname)
 
 print(hostname)
 print(IP)
+print(allowed_hosts)
 
-'''
-ipMio = getLocalAddress()
-ipHost = dirsolve(name)
-print ipHost
-print ipMio'''
-
-httpd.serve_forever()
-
-net = tcpStream()
-net.open(HOST, PORT)
-
-ipHost = dirsolve(name)
-ipMio = getLocalAddress()
-
-print ipHost
-print ipMio
-
-if(ipMio != ipHost):
-    print 'sono diversi'
+if(IP != allowed_hosts):
+    print 'non puoi accedere a questo sito'
 else:
-    print 'sono uguali'
-
-response = urllib2.urlopen()
-print response
-
-header = response.info()
-print 'Data: ', header['date']
-print 'Header: ', header
-
-data = response.read()
-print 'Dati: '
-
-print 'Url: ', pathname2url(path)
-print 'Percorso: ', url2pathname('/d/e/f')
+    print 'puoi accedere'
+    httpd.serve_forever()
